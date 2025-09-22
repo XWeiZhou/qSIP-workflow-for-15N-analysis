a9962284960e9d648748ae7d349f843d6772b112
XWeiZhou <46567527+XWeiZhou@users.noreply.github.com>
2025-09-22 11:21:00 +0800

    Add files via upload

---
+++ 2. Extract moss active OTU IDs for phylogenetic tree pipeline.R
@@ -0,0 +1,57 @@
+# load the packages
+library(dplyr)
+library(clipr)
+library(tidyr)
+library(readxl)
+
+#set work directory
+setwd("C:/Users/Lenovo/Desktop/2025年9月PNAS文章修改/上传代码和数据/数据/")
+
+# Read the filtered Excel file instead of CSV
+data <- read_excel("filtered_data_taxa_pro_signi_minus_0_20250330_1000.xlsx", sheet = 1)
+data$eaf <- as.numeric(data$eaf)
+
+# Convert the Treatment column to a factor variable
+data$treatment <- factor(data$treatment, levels = c("Control","Warming"))
+
+# Calculate the mean of EAF
+result <- data %>%
+  filter(ecosystem == "moss" & eaf > 0) %>%
+  group_by(asv_id, treatment) %>%
+  summarise(n =length(eaf), mean_value = mean(eaf))
+
+# Use the pivot_wider function to convert long-format data (df) to wide-format
+result <- result[,-3]
+wide_result <- pivot_wider(data = result, names_from = treatment, values_from = mean_value)
+wide_result
+str(wide_result)
+
+# Replace all NA values with 0
+wide_result[is.na(wide_result)] <- 0
+
+# Add a column for the EAF difference
+wide_result$delta.eaf <- wide_result$Warming - wide_result$Control 
+  
+# load all taxa dataset
+taxa <- read.csv("taxa.csv")
+names(taxa) <- c("asv_id","Kingdom","Phylum","Class","Order","Family","Genus", "Species")
+
+# Compare the two datasets and retain the table structure for OTUs that have EAF values in both
+merge_result <- merge(wide_result, taxa, by = "asv_id", all.x = TRUE)
+annotation <- data.frame(merge_result)
+annotation
+
+# Since Warming appears before Control, the second column is Warming instead of Control; 
+# check the data carefully when naming columns
+names(annotation) <- c("OTUID","Warming","Control","delta.eaf","Kingdom","Phylum","Class","Order","Family","Genus", "Species")
+write.csv(annotation, "annotation_moss.csv")
+
+# Output selected OTU names for later comparison with otus_taxa.fa, 
+# to extract OTU sequences for tree construction
+select_OTU_names <- as.data.frame(annotation[,1])
+names(select_OTU_names) <- c("asv_id")
+write.table(select_OTU_names, "select_OTU_moss_509.txt", sep = "/t", row.names = F, col.names = T, quote = F)
+
+# Check the dimensions (rows and columns) of the data
+data.moss <- read.csv("annotation_moss.csv")
+dim(data.moss)
---
+++ 3. Phylogenetic tree construction.sh
@@ -0,0 +1,72 @@
+
+#!/bin/bash
+    # Set working directory (wd) and software/database (db) directory
+    # Add environment variables and enter wd
+    # **The following four lines must be run every time RStudio is opened**
+    wd=/c/A20250331MossSIPTree/A20250331MossSIPTree_all_OTUS
+    db=/c/public
+    PATH=$PATH:${db}/win
+    cd ${wd}
+
+# 1、Evolutionary Tree worddirectory
+    cd ${wd}
+    
+## Match the ID column names selected after SIP-EAF filtering with all OTU.fa sequences,
+## extract the corresponding gene sequences for tree construction
+  seqkit grep -f annotation_moss_all_OTUS.txt all_taxa.fa > select_OTU_moss.fa 
+  
+## 2. Phylogenetic tree construction
+    # Input files are located in result/tree directory: 
+    #   - otus.fa (sequences) 
+    #   - annotation.txt (taxonomy and relative abundance)
+    # Step 1: Perform sequence alignment with MUSCLE
+    muscle -in select_OTU_moss.fa -out otus_aligned_moss.fas
+
+    # Construct a maximum likelihood phylogenetic tree using IQ-TREE
+    # -s   specify aligned sequence input file
+    # -bb  set bootstrap resampling to 1000 replicates
+    # -alrt set ALRT (Approximate Likelihood-Ratio Test) resampling to 1000 replicates
+    # -nt  AUTO: automatically choose number of threads based on available CPUs
+    # -pre specify output file prefix
+    # -redo rerun analysis even if output files already exist
+    
+    mkdir -p iqtree
+    iqtree -s otus_aligned_moss.fas \
+        -bb 1000 -redo -alrt 1000 -nt AUTO \
+        -pre iqtree/select_OTU_moss
+
+## 3. Tree visualization and annotation
+
+    # Visit http://itol.embl.de/, upload otus.nwk, 
+    # then drag-and-drop the generated annotation schemes onto the tree for visualization.
+
+
+    ## Scheme 1. Outer-ring colors, shapes, taxonomy, and abundance
+    # annotation.txt contains OTU taxonomy annotation and abundance.
+    # -a stop if input column not found (default: disabled)
+    # -c convert integer columns to factors or decimals to numeric
+    # -t convert ID column if label mismatch occurs
+    # -w control width of color bands/areas
+    # -D output directory
+    # -i OTU column name
+    # -l OTU display label (e.g., species/genus/family name)	
+    Rscript ${db}/script/table2itol.R -a -c double -D plan1 -i OTUID -l Genus -t %s -w 0.5 annotation_moss.txt
+    # Each column in the annotation file will be exported as a separate file
+
+    ## Scheme 2. Generate abundance bar plot annotation files
+    Rscript ${db}/script/table2itol.R -a -d -c none -D plan2 -b Phylum -i OTUID -l Genus -t %s -w 0.5 annotation_moss.txt
+    Rscript ${db}/script/table2itol.R -a -d -c none -D plan2_2 -b Phylum -i OTUID -l Phylum -t %s -w 0.5 annotation_moss.txt
+
+    ## Scheme 3. Generate heatmap annotation files
+    Rscript ${db}/script/table2itol.R -c keep -D plan3 -i OTUID -t %s otutab_moss.txt
+
+    ## Scheme 4. Convert integer values to factors and generate annotation files
+    Rscript ${db}/script/table2itol.R -a -c factor -D plan4 -i OTUID -l Genus -t %s -w 0 annotation_moss.txt
+
+    # Visualize iqtree/otus.contree on http://itol.embl.de/ 
+    # and drag files from different Plans onto the tree for annotation
+
+    # Return to working directory
+    cd ${wd}
+    
+###################################################################################
